c207186dd0531e2805af8105ccf9e9b6ccdaeb0c
wk3368 <wk3368@163.com>
2021-04-08 15:10:48 +0800

    maintain version in package.json only

--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,4 @@
-export const version = '0.1.7';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('../package.json');
+
+export const { version } = packageJson;
