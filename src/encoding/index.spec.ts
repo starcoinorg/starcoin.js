da4d146ba097a278d90ef94365502457a1fa9a76
wk3368 <wk3368@163.com>
2021-04-24 16:54:39 +0800

    add a test case: "decoding SignedUserTransaction hex"

--- src/encoding/index.spec.ts
+++ src/encoding/index.spec.ts
@@ -1,4 +1,4 @@
-import { addressToSCS, decodeEventKey, decodeTransactionPayload } from '.';
+import { addressToSCS, decodeEventKey, decodeTransactionPayload, decodeSignedUserTransaction } from '.';
 
 test("encoding address", () => {
   expect(addressToSCS("0x1").value.length).toBe(16);
@@ -21,3 +21,13 @@ test("decoding EventKey", () => {
   console.log(eventKey);
 });
 
+test("decoding SignedUserTransaction hex", () => {
+  const hex = "0x49624992dd72da077ee19d0be210406a100000000000000002000000000000000000000000000000010f5472616e73666572536372697074730c706565725f746f5f706565720107000000000000000000000000000000010353544303535443000310621500bf2b4aad17a690cb24f9a225c601001000ca9a3b0000000000000000000000001fe501000000000001000000000000000d3078313a3a5354433a3a535443e8ab000000000000fe002020e2c9a32b0ce41c3a5f4a5f010909741f12e265debcb681c9f9d58c2e69e65c4040288d5662f0f0e72d181073c00bd4f6a15fcfaf4911f6ac35827e09c5e6e02d0df0f2d1bb81c90617911362a39801b88cf6ae405ef226c2e6645a1e5c946e09";
+  const signedUserTransaction = decodeSignedUserTransaction(hex);
+  // console.log(signedUserTransaction)
+  expect(signedUserTransaction.hasOwnProperty("transaction_hash")).toBeTruthy();
+  expect(signedUserTransaction.hasOwnProperty("raw_txn")).toBeTruthy();
+  expect(signedUserTransaction.hasOwnProperty("authenticator")).toBeTruthy();
+  expect(signedUserTransaction.transaction_hash).toBe("0x17d671d18736358b7e6665be3cde9d27a4e515f5f12ce9d014f45ff550be84d3");
+  expect(signedUserTransaction.raw_txn.sender).toBe("0x49624992dd72da077ee19d0be210406a");
+});
