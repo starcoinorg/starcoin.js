e918669e25957db7a35a065d2cc713936d222024
wk3368 <wk3368@163.com>
2021-04-25 12:22:53 +0800

    generate args of ScriptFunction is ok

--- src/encoding/index.spec.ts
+++ src/encoding/index.spec.ts
@@ -1,14 +1,14 @@
 import { addressToSCS, decodeEventKey, decodeTransactionPayload, decodeSignedUserTransaction } from '.';
-import { BcsSerializer } from '../lib/runtime/bcs';
+import { BcsSerializer, BcsDeserializer } from '../lib/runtime/bcs';
 import { toHexString } from '../utils/hex';
-import { encodeScriptFunction } from "../utils/tx";
+import { encodeScriptFunction, encodeSignedUserTransaction } from "../utils/tx";
 
 test("encoding address", () => {
   expect(addressToSCS("0x1").value.length).toBe(16);
   expect(addressToSCS("0x01").value.length).toBe(16);
 });
 
-test("generate TransactionPayload->ScriptFunction hex", () => {
+test("encoding TransactionPayload->ScriptFunction hex", () => {
 
   const functionId = '0x1::TransferScripts::peer_to_peer'
 
@@ -50,13 +50,28 @@ test("decoding EventKey", () => {
   console.log(eventKey);
 });
 
+test("encoding SignedUserTransaction hex", async () => {
+
+  const senderPrivateKeyHex = '0x83c7829c68e1ad81ced10f69d11ea741f7f18c7a5f059215e8a965362a5ae25e'
+  // TODO: generate address using privateKey
+  const senderAddressHex = '0x49624992dd72da077ee19d0be210406a'
+
+  const receiverAddressHex = '0x621500bf2b4aad17a690cb24f9a225c6'
+
+  const amount = 1000000000
+
+  const maxGasAmount = 124191
+
+  const hex = await encodeSignedUserTransaction(senderPrivateKeyHex, senderAddressHex, receiverAddressHex, amount, maxGasAmount);
+
+  const hexExpected = "0x49624992dd72da077ee19d0be210406a100000000000000002000000000000000000000000000000010f5472616e73666572536372697074730c706565725f746f5f706565720107000000000000000000000000000000010353544303535443000310621500bf2b4aad17a690cb24f9a225c601001000ca9a3b0000000000000000000000001fe501000000000001000000000000000d3078313a3a5354433a3a535443e8ab000000000000fe002020e2c9a32b0ce41c3a5f4a5f010909741f12e265debcb681c9f9d58c2e69e65c4040288d5662f0f0e72d181073c00bd4f6a15fcfaf4911f6ac35827e09c5e6e02d0df0f2d1bb81c90617911362a39801b88cf6ae405ef226c2e6645a1e5c946e09";
+  expect(hex).toBe(hexExpected);
+});
+
 test("decoding SignedUserTransaction hex", () => {
   const hex = "0x49624992dd72da077ee19d0be210406a100000000000000002000000000000000000000000000000010f5472616e73666572536372697074730c706565725f746f5f706565720107000000000000000000000000000000010353544303535443000310621500bf2b4aad17a690cb24f9a225c601001000ca9a3b0000000000000000000000001fe501000000000001000000000000000d3078313a3a5354433a3a535443e8ab000000000000fe002020e2c9a32b0ce41c3a5f4a5f010909741f12e265debcb681c9f9d58c2e69e65c4040288d5662f0f0e72d181073c00bd4f6a15fcfaf4911f6ac35827e09c5e6e02d0df0f2d1bb81c90617911362a39801b88cf6ae405ef226c2e6645a1e5c946e09";
   const signedUserTransaction = decodeSignedUserTransaction(hex);
-  // console.log(signedUserTransaction)
-  expect(signedUserTransaction.hasOwnProperty("transaction_hash")).toBeTruthy();
-  expect(signedUserTransaction.hasOwnProperty("raw_txn")).toBeTruthy();
-  expect(signedUserTransaction.hasOwnProperty("authenticator")).toBeTruthy();
+  console.log(signedUserTransaction)
   expect(signedUserTransaction.transaction_hash).toBe("0x17d671d18736358b7e6665be3cde9d27a4e515f5f12ce9d014f45ff550be84d3");
   expect(signedUserTransaction.raw_txn.sender).toBe("0x49624992dd72da077ee19d0be210406a");
 });
--- src/utils/tx.ts
+++ src/utils/tx.ts
@@ -1,8 +1,9 @@
 import { bytes } from '../lib/runtime/serde';
 import * as starcoin_types from '../lib/runtime/starcoin_types';
-import { FunctionId, HexString, parseFunctionId, TypeTag } from '../types';
-import { addressToSCS, typeTagToSCS } from '../encoding';
-import { arrayify } from '@ethersproject/bytes';
+import { BcsDeserializer, BcsSerializer } from '../lib/runtime/bcs';
+import { FunctionId, HexString, parseFunctionId, TypeTag, U128 } from '../types';
+import { addressFromSCS, addressToSCS, typeTagToSCS } from '../encoding';
+import { arrayify, hexlify } from '@ethersproject/bytes';
 
 export function encodeTransactionScript(
   code: bytes,
@@ -18,8 +19,8 @@ export function encodeTransactionScript(
 }
 
 export function encodeScriptFunction(functionId: FunctionId,
-                                     tyArgs: TypeTag[],
-                                     args: HexString[]): starcoin_types.TransactionPayloadVariantScriptFunction {
+  tyArgs: TypeTag[],
+  args: HexString[]): starcoin_types.TransactionPayloadVariantScriptFunction {
   let funcId = parseFunctionId(functionId);
   const scriptFunction = new starcoin_types.ScriptFunction(
     new starcoin_types.ModuleId(addressToSCS(funcId.address), new starcoin_types.Identifier(funcId.module)),
@@ -49,3 +50,49 @@ export function encodePackage(
     packageData
   );
 }
+
+export async function encodeSignedUserTransaction(
+  senderPrivateKey: HexString,
+  senderAddress: HexString,
+  receiverAddress: HexString,
+  amount: U128,
+  maxGasAmount: U128
+): Promise<string> {
+  // TODO: check the receiver exists on the chain or not
+  // assuming the receiver exists on the chain already
+  const receiverAuthKeyHex = '0x00'
+
+  const functionId = '0x00000000000000000000000000000001::TransferScripts::peer_to_peer'
+
+  const address = '0x1';
+  const module = 'STC';
+  const name = 'STC';
+  const type_params = [];
+  const tyArgs = [{ Struct: { address, module, name, type_params } }]
+
+  // Multiple BcsSerializers should be used in different closures, otherwise, the latter will be contaminated by the former.
+  const amountSCSHex = (function () {
+    const se = new BcsSerializer();
+    se.serializeU128(BigInt(amount));
+    return hexlify(se.getBytes());
+  })();
+
+  const args = [
+    receiverAddress,
+    receiverAuthKeyHex,
+    amountSCSHex
+  ]
+
+  const scriptFunction = encodeScriptFunction(functionId, tyArgs, args);
+
+  const payloadInHex = (function () {
+    const se = new BcsSerializer();
+    scriptFunction.serialize(se);
+    return hexlify(se.getBytes());
+  })();
+
+  console.log(payloadInHex)
+
+  return "0x49624992dd72da077ee19d0be210406a100000000000000002000000000000000000000000000000010f5472616e73666572536372697074730c706565725f746f5f706565720107000000000000000000000000000000010353544303535443000310621500bf2b4aad17a690cb24f9a225c601001000ca9a3b0000000000000000000000001fe501000000000001000000000000000d3078313a3a5354433a3a535443e8ab000000000000fe002020e2c9a32b0ce41c3a5f4a5f010909741f12e265debcb681c9f9d58c2e69e65c4040288d5662f0f0e72d181073c00bd4f6a15fcfaf4911f6ac35827e09c5e6e02d0df0f2d1bb81c90617911362a39801b88cf6ae405ef226c2e6645a1e5c946e09";
+}
+
