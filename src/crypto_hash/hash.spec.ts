9d4fc13815bcf40b6889993f13018401e2faca0a
Bingo Du <tarvos21@gmail.com>
2021-04-29 18:24:39 +0800

    Add test case for converting public key to auth key (on barnard network)

--- src/crypto_hash/hash.spec.ts
+++ src/crypto_hash/hash.spec.ts
@@ -1,4 +1,4 @@
-import { createHash } from ".";
+import { createHash, publicKeyToAuthKey } from ".";
 
 describe("crypo hash", () => {
   test("should hash correct", () => {
@@ -7,6 +7,15 @@ describe("crypo hash", () => {
     const value = hasher.crypto_hash(data);
     expect(value).toBe(
       "0x7875f210ee3a08253dc4cdfde9ea7c170ec7dca3866c819c622e671ce7df5d60"
+    );
+  });
+  test("should convert public key to auth key correctly", () => {
+    // work on barnard network
+    const public_key = '0xc51dada886afe59d4651f36b56f3c4a1a84da53dfbddf396d81a5b36ab5cdc26'
+    const value = publicKeyToAuthKey(public_key)
+    console.log({auth_key: value})
+    expect(value).toBe(
+      "0xc804cb6c5652488f61386a0107bc88cd3f19d5422824f47e6c021978cee98f35"
     );
   });
 });
