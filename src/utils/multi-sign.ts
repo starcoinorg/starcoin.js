4c633abc69a2e42c0289feca46e4b907047d0b3c
wk3368 <wk3368@163.com>
2021-09-30 17:27:18 +0800

    1. merge privateKeys' publicKey into publicKeys 2. generate pub->priv map

--- src/utils/multi-sign.ts
+++ src/utils/multi-sign.ts
@@ -14,6 +14,33 @@ import { showAccount } from "./account"
  * @returns 
  */
 export async function createMultiSignAccount(publicKeys: Array<string>, privateKeys: Array<string>, thresHold: number): Promise<Record<string, string>> {
+  if (privateKeys.length === 0) {
+    throw new Error('require at least one private key');
+  }
+
+  const pubPrivMap = {}
+
+  // 1. merge privateKeys' publicKey into publicKeys
+  // 2. generate pub->priv map
+  await Promise.all(
+    privateKeys.map((priv) => {
+      return privateKeyToPublicKey(priv).then((pub) => {
+        publicKeys.push(pub);
+        pubPrivMap[pub] = priv;
+        return pub;
+      }).catch((error) => {
+        throw new Error(`invalid private key: ${ error }`)
+      })
+    })
+  )
+
+  console.log(publicKeys);
+  console.log(pubPrivMap);
+
+  // sort all public keys by its bytes to make sure same public key set always generate same auth key.
+
+  // remove repeat public keys, if use add repeat public_key or private key.
+
   const accountInfo = showAccount(privateKeys[0]);
   return accountInfo;
 }
